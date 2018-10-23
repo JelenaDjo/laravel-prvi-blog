5a292b7a79fcf6dcac1b01be8cbda616c3d94f71
JelenaDJo <djokicj@yahoo.com>
2018-10-23 22:07:42 +0200

    Delete comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-  public function store ($id)
+  public function store ($postId)
   {
-      $post= Post::findOrFail($id);
+      $post= Post::findOrFail($postId);
 
       $this->validate(
         request(),
@@ -21,7 +21,16 @@ class CommentsController extends Controller
       $post->comments()->create(
           request()->all()
       );
-      return redirect("/posts/{$id}"); // moraju dupli navodnici a moze i bez viticastih oko id 
+      return redirect("/posts/{$postId}"); // moraju dupli navodnici a moze i bez viticastih oko id 
       //return redirect('/posts/' .$id);  // moze i ovako da se napise ovo gore
   }  
+  public function destroy ($postId, $commentId)
+  {
+      $comment = Comment::findOrFail($commentId);
+      $comment->delete();
+
+      return redirect("/posts/{$postId}");
+     
+     // dd(compact(['postId', 'commentId']));
+  }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,8 +25,17 @@
             <li>
                 <p><strong>Author:</strong> {{$comment->author}}</p>
                 <p> {{ $comment->text}} </p>
+
+            <form method="POST" action="/posts/{{ $post->id }}/comments/{{ $comment->id }}">
+            {{ csrf_field() }}
+
+             <button type="submit" class="btn btn-danger">Delete comment</button>
+            </forma>
+
             </li>
             @endforeach
+
+           
         </ul>
     @endif
 
