fa5d4997eca54be1e66e46d3b018c0011a9a0239
JelenaDJo <djokicj@yahoo.com>
2018-10-25 21:03:23 +0200

    Login User

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,21 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    public function index()
+    {
+        return view('login.index');
+    }
+    public function login()
+
+    {
+        if(!auth()->attempt(request(['email', 'password']))) {//attempt proverava da li u bazi vec postoji korisnik sa tim emailom ili passwordom){
+            return back()->withErrors([
+                'message'=> 'Nece da moze prijatelju!'
+            ]);
+        }
+            return redirect ('/posts');
+    }
+    
     public function logout()
     {
         auth()->logout();
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -4,27 +4,29 @@
     Create post
 @endsection
 
-@section('content')
-<h2>Registration </h2>
 
-<form method="POST" action="/register">
+@section('content')
+
+<h2> Login Users</h2>
+<form method="POST" action="/login">
     {{ csrf_field() }}
 
-        <div class="form-group">
-            <label>Name</label>
-            <input name="name" type="text" class="form-control"  placeholder="Enter name">
-            @include('layouts.partials.error-message', ['field'=>'name'])
-        </div>
+       
         <div class="form-group">
             <label>Email</label>
             <input name="email" type="text" class="form-control" placeholder="Enter email">
-            @include('layouts.partials.error-message', ['field'=>'email'])
         </div>
         <div class="form-group">
             <label>Password</label>
             <input name="password" type="password" class="form-control"  placeholder="Enter password">
-            @include('layouts.partials.error-message', ['field'=>'password'])
         </div>
      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    @if(count($errors->all()))
+        @foreach($errors->all() as $error)
+        <div class="alter alter-danger">
+            {{ $error }}
+        </div> 
+        @endforeach
+    @endif    
 @endsection
